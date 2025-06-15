7b7155860f7f649cae4cdc8d50056333f4877a07
ollyjohnson <ollyjohnson@icloud.com>
2025-06-15 21:53:26 +0100

    Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import {ClerkProviderWithRoutes} from "./"
+import {Routes, Route} from "react-router-dom"
+import {Layout} from "./layout/layout.jsx"
+import {TradeLog} from "./trades/TradeLog.jsx"
+import { HistoryPanel } from './history/HistroyPanel.jsx'
+import { AuthenticationPage } from "./auth/AuthenticationPage.jsx"
+import './App.css'
+
+function App() {
+  return <ClerkProviderWithRoutes>
+    <Routes>
+      <Route path="/sign-in/*" element={<AuthenticationPage />}/>
+      <Route path="/sign-up" element={<AuthenticationPage />}/>
+      <Route element = {<Layout />}> 
+        <Route path ="/" element={<TradeLog />}/>
+        <Route path ="/history" element={<HistoryPanel />}/>
+      </Route>
+    </Routes>
+  </ClerkProviderWithRoutes>
+}
+
+export default App
